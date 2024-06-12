a36a5051c4b7ffc5d67bb12f3b8bd73076a93c70
Rodrigo-S-Luz <rdg.roodrigo@gmail.com>
2024-06-11 22:08:00 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const cors = require('cors')
+app.use(cors())
+app.use(express.json())
+
+//banco de dados em memória
+var clientes = []
+
+app.get('/listar', (request, response) => {
+  response.json(clientes)
+})
+
+app.get('/listar', (request, response) => {
+  response.json(clientes)
+  .then((clientes) => {
+    response.json(JSON.parse(clientes))
+  }).catch(() => {
+    response.json([])
+  })  
+})
+
+app.post("/cadastrar", (request, response) => {
+    let cliente = request.body
+    console.log(cliente)
+    clientes.push(cliente) //adiciona o cliente no BD
+    response.json({ success: true  })
+})
+
+app.delete("/excluir/:cpf", (request, response) => {
+  let cpf = request.params.cpf
+  for(let i=0; i < clientes.length; i++){
+    let cliente = clientes[i]
+    if (cliente.cpf == cpf){
+        //remove o elemento encontrado na posição "i"
+        clientes.splice(i, 1) 
+    }
+  }
+  response.json({ success: true })
+})
+
+app.put("/alterar", (request, response) => {
+  let cliente = request.body;
+//procura o cliente que tem o CPF enviado
+  for(let i=0; i < clientes.length; i++){
+    if (clientes[i].cpf == cliente.cpf){
+      //Substitui os dados do cliente pelos dados enviados pelo front
+        clientes[i] = cliente
+    }
+  }
+  response.json({ success: true })
+})
+
+app.get('/buscar', (request, response) => {
+  const searchTerm = request.query.q.toLowerCase();
+  const filteredClientes = clientes.filter(cliente => {
+    return cliente.nome.toLowerCase().includes(searchTerm) ||
+           cliente.cpf.includes(searchTerm) ||
+           cliente.telefone.includes(searchTerm) ||
+           cliente.whatsapp.includes(searchTerm) ||
+           cliente.sexoCalopsita.toLowerCase().includes(searchTerm);
+  });
+  response.json(filteredClientes);
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
